daf74cdf3b5d80f9d8ea40650e935303e0eb066e
teologo-x <97321897+teologo-x@users.noreply.github.com>
2023-04-14 19:20:10 -0300

    TP1
    Subido el tp1

---
+++ TP1/JS/funciones.js
@@ -0,0 +1,15 @@
+
+
+
+
+function miFuncion() {
+
+    const nom=document.getElementById("name");
+    const apell=document.getElementById("lastname");
+    const edad=document.getElementById("age");
+
+    alert("Nombre: "+nom.value+"\nApellido: "+apell.value+"\nEdad: "+edad.value);
+    
+}
+
+
